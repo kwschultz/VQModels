ca6e1a034225ffc3edc7fd2cbf474d65c6fcad44
kwschultz <kwschultz@ucdavis.edu>
2016-03-03 17:54:39 -0800

    Updated

--- UCERF3/eqsim_to_vq.sh
+++ UCERF3/eqsim_to_vq.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-    echo "$0 <element size>"
+    echo "$1 <taper method>"
+    echo "$2 <stress drop factor>"
     exit -1
 fi
 
-ELEM_SIZE=$1
+TAPER=$1
+STRESS_DROP_FACTOR=$2
 
-GEOFILE='UCERF3_EQSIM_FM3_1_ZENGBB_Geometry.dat'
-FRICFILE='UCERF3_EQSIM_Friction.dat'
+GEOFILE='UCERF3_EQSim_ReIndexed_AseismicCut_0.11_Geometry.dat'
+FRICFILE='UCERF3_EQSim_ReIndexed_AseismicCut_0.11_Friction.dat'
+OUTNAME='UCERF3_VQmeshed_from_EQSIM_ReIndexed_AseismicCut_0-11'
 
-EDITOR_ARGS="--import_eqsim_geometry=$GEOFILE --import_eqsim_friction=$FRICFILE"
-#EDITOR_ARGS="--import_eqsim_geometry=$GEOFILE"
 
-../../vq/build/src/mesher $EDITOR_ARGS \
-    --export_file=UCERF3_fault_${ELEM_SIZE}.txt \
+EDITOR_ARGS="--import_eqsim_geometry=$GEOFILE --import_eqsim_friction=$FRICFILE --taper_fault_method=$TAPER"
+
+~/vq/build/src/mesher $EDITOR_ARGS \
+    --export_file=${OUTNAME}_${TAPER}_drops${STRESS_DROP_FACTOR}.txt \
     --export_file_type=text \
-    --export_file=UCERF3_fault_${ELEM_SIZE}.kml \
-    --export_file_type=kml --merge_duplicate_verts
-    #--export_eqsim_geometry=UCERF3_${ELEM_SIZE}_EQSIM_out.dat \
-    
-    
-    
-#     --export_file=UCERF3_fault_${ELEM_SIZE}.h5 \
-#     --export_file_type=hdf5 \
-
+    --export_file=${OUTNAME}_${TAPER}_drops${STRESS_DROP_FACTOR}.h5 \
+    --export_file_type=hdf5 \
+    --export_file=${OUTNAME}_${TAPER}.kml \
+    --export_file_type=kml \
+    --merge_duplicate_verts \
+    --stress_drop_factor=${STRESS_DROP_FACTOR} 
