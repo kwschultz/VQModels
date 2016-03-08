550204813f7f7b319aa566f1e60a785291a1e73f
kwschultz <kwschultz@ucdavis.edu>
2016-03-08 15:49:03 -0800

    Updated eqsim to vq mesher script for UCERF2

--- UCERF2/eqsim_to_vq.sh
+++ UCERF2/eqsim_to_vq.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 
-#if [ $# -ne 1 ]
-#then
-#    echo "$0 <element size>"
-#    exit -1
-#fi
+if [ $# -ne 2 ]
+then
+    echo "$1 <taper method>"
+    echo "$2 <stress drop factor>"
+    exit -1
+fi
 
 #ELEM_SIZE=$1
 #KM=$(($ELEM_SIZE / 1000))
 
+TAPER=$1
 GEOFILE='ALLCAL2_1-7-11_no-creep/ALLCAL2_1-7-11_no-creep_Geometry.dat'
 FRICFILE='ALLCAL2_1-7-11_no-creep/ALLCAL2_1-7-11_no-creep_Friction.dat'
+OUTNAME='UCERF2_VQmeshed_3km_preTapered_noCreep'
+STRESS_DROP_FACTOR=$2
 
 EDITOR_ARGS="--import_eqsim_geometry=$GEOFILE --import_eqsim_friction=$FRICFILE"
 
 ~/vq/build/src/mesher $EDITOR_ARGS \
-    --export_file=ALLCAL2_VQmeshed_3km.txt \
+    --export_file=${OUTNAME}_${TAPER}_drops${STRESS_DROP_FACTOR}.txt \
     --export_file_type=text \
-    --export_file=ALLCAL2_VQmeshed_3km.h5 \
+    --export_file=${OUTNAME}_${TAPER}_drops${STRESS_DROP_FACTOR}.h5 \
     --export_file_type=hdf5 \
-    --export_file=ALLCAL2_VQmeshed_3km_test_red.kml \
+    --export_file=${OUTNAME}_${TAPER}.kml \
     --export_file_type=kml \
-    --merge_duplicate_verts \
-    --delete_unused 
+    --taper_fault_method=${TAPER} \
+    --stress_drop_factor=${STRESS_DROP_FACTOR}  \
 
