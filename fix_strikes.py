a8435518becf3456e9f50f320247642a9e396854
kwschultz <kwschultz@ucdavis.edu>
2016-05-02 18:16:18 -0700

    Improved script that fixes strikes, fixed 3 new faults.

--- fix_strikes.py
+++ fix_strikes.py
@@ -2,21 +2,25 @@ import numpy as np
 import re, math
 from matplotlib import pyplot as plt
 
-SECTION_FILE = 'section_strikes_SAF_fix_LA_Basin_and_Mojave_Fix.txt'
+
+### !!!!!! First thing!!! We want the finalized file containing strikes to be named 
+## section_strikes_with_fixes.txt, so go and rename the current one to something else
+SECTION_FILE = 'section_strikes_with_fixes_previous.txt'
 FIXED_SECTION_STRIKES = 'section_strikes_with_fixes.txt'
 
-## Faults that have been fixed to have correct strikes:
-# San Andreas, Garlock, Raymond, Hollywood, Santa Monica, Palos Verdes, 
-
-
+## Faults that have been fixed to have correct strikes: listed in verified_and_fixed_faults.txt 
 section_strike_file = open(SECTION_FILE,'r')
 new_section_strike_file = open(FIXED_SECTION_STRIKES,'w')
+
+## This implementation works for matching the unique word in the section name before the first
+## underscore. I.e. "Bartlett" matches "Bartlett_Springs_2011_CFM_Subsection_0" 
+UNIQUE_SECTION_SUB_NAMES = ["Hunting","Bartlett"]
 
 
 reg = '\_Subsection\_[0-9]+$'  # trim the "Subsection" and underscore and the trailing number
 
 
-#######  UTILITIES ------------------------------------------
+#######  UTILITIES --------------------------------------
 def dotproduct(v1, v2):
   return sum((a*b) for a, b in zip(v1, v2))
 
@@ -50,24 +54,15 @@ def strike_difference_angle(strike1, strike2):
     return 180.0*angle(vector1, vector2)/np.pi
     
 # These functions take a list of strikes (as a string) and return
-#  a list of corrected strikes (as a string).
-def add_180(string):
-    ray = [ float(x)+180 for x in string.split()]
+#  a list of corrected strikes (as a string). 
+def flip_strike(string):
+    ray = [ (float(x)+180)%360 for x in string.split()]
     line = ""
     for val in ray:
         line+=str(val)+" "
     return line
-    
-    
-def subtract_180(string):
-    ray = [ float(x)-180 for x in string.split()]
-    line = ""
-    for val in ray:
-        line+=str(val)+" "
-    return line
-    
-    
-#######  UTILITIES ------------------------------------------
+####### ------------------------------------------
+
 
 
 
@@ -75,9 +70,12 @@ for line in section_strike_file:
     name, secs = line.split(" = ")
     trimmed_name = re.sub(reg,'', name)
     trimmed_and_split = trimmed_name.split("_")
-    if trimmed_and_split[0] == "Garlock":
-        # I know apriori that I need to subtract 180
-        line_to_write = name+" = "+subtract_180(secs)+"\n"
+    ##############################
+    ##############################
+    #### Modify the line below if you need to match, for example "name1_name2"
+    ##############################
+    if trimmed_and_split[0] in UNIQUE_SECTION_SUB_NAMES:
+        line_to_write = name+" = "+flip_strike(secs)+"\n"
     else:
         line_to_write = line
     
@@ -86,4 +84,5 @@ for line in section_strike_file:
 
 section_strike_file.close()
 new_section_strike_file.close()
+print("New strike data written to "+FIXED_SECTION_STRIKES)
     
